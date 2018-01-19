202522bff3860a9d59932fa4b365988559550a47
Charlie Gallentine <chgallentine@gmail.com>
2018-01-19 13:56:02 -0500

    Added random background color event

--- javascript/index.js
+++ javascript/index.js
@@ -11,5 +11,23 @@ downloadResume = () => {
 	});
 }
 
-downloadResume();
+function setColor(e) {
+  let x;
+  let y;
+  let red = 223;
+  let green = 131;
+  let blue = 51;
+  
+  x = e.clientX;
+  y = e.clientY;
+  
+  red = (x + y) % 255;
+  green = x % 255;
+  blue = y % 255;
+  
+  console.log(red, green, blue);
+  document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}
 
+downloadResume();
+document.addEventListener("click", setColor);
