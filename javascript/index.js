2abe02ba348eeb3ca05f8815e12469da7defc28c
Charlie Gallentine <chgallentine@gmail.com>
2018-01-22 14:33:00 -0500

    added jquery

--- javascript/index.js
+++ javascript/index.js
@@ -21,6 +21,10 @@ downloadConfirmation = (e) => {
   return false;
 }
 
+document.getElementById('about').scrollIntoView({ 
+  behavior: 'smooth' 
+});
+
 function setColor(e) {
   let x;
   let y;
@@ -53,6 +57,42 @@ function setColor(e) {
   	color2Link[i].style.borderBottomColor = `rgb(${(294-red) % 255}, ${255-green}, ${(161-blue)%255})`;
   }
 }
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
 changeResumeText();
 document.getElementById("download").addEventListener("click", downloadConfirmation);
