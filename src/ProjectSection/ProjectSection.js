b144956619b5fc18b2b994531c0127fb90ecf9f6
Charlie Gallentine <chgallentine@gmail.com>
2018-07-26 20:59:33 -0600

    Converted to react

---
+++ src/ProjectSection/ProjectSection.js
@@ -0,0 +1,22 @@
+/*
+* @Author: Charlie Gallentine
+* @Date:   2018-07-25 21:30:18
+* @Last Modified by:   Charlie Gallentine
+* @Last Modified time: 2018-07-25 21:31:21
+*/
+import React, { Component } from 'react';
+import './ProjectSection.css';
+
+export default class ProjectSection extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				Project Section
+            </div>
+		);
+	}
+};
