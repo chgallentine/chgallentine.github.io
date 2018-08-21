650edec6fddfe976360561bd99d6c4baeb4b0320
Charlie Gallentine <chgallentine@gmail.com>
2018-08-21 15:01:53 -0400

    Added the things I screwed up

---
+++ src/ContactSection/ContactSection.js
@@ -0,0 +1,46 @@
+/*
+* @Author: Charlie Gallentine
+* @Date:   2018-07-24 21:46:34
+* @Last Modified by:   Charlie Gallentine
+* @Last Modified time: 2018-08-20 16:55:23
+*/
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+import './ContactSection.css';
+
+export default class ContactSection extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+			<div id="media_links">
+	              <a href="https://github.com/chgallentine" 
+	              	class="link_to_media"
+	                target="_blank"
+	                rel="noopener noreferrer"
+	                style={{color: this.props.colors.backgroundColor}}>
+	                <FontAwesome name="fa-github" className="fa fa-github" aria-hidden="true" />
+	              </a>
+	              <a href="https://www.linkedin.com/in/charlie-gallentine-0a0424a2/" 
+	              	class="link_to_media"	
+	                target="_blank"
+	                rel="noopener noreferrer"
+	                style={{color: this.props.colors.backgroundColor}}>
+	                <FontAwesome name="fa-linkedin" className="fa fa-linkedin" size="6em" aria-hidden="true"/>
+	              </a>
+	              <a href="https://www.instagram.com/chgallentine/" 
+	              	class="link_to_media"
+	                target="_blank"
+	                rel="noopener noreferrer"
+	                style={{color: this.props.colors.backgroundColor}}>
+	                <FontAwesome name="fa-instagram" className="fa fa-instagram" aria-hidden="true"/>
+	              </a>
+	        </div>
+	            <h3 style={{textAlign:'center', color:'white',}}>Feel free to reach out!</h3>
+            </div>
+		);
+	}
+};
---
+++ src/ProjectSection/ProjectSection.js
@@ -0,0 +1,71 @@
+/*
+* @Author: Charlie Gallentine
+* @Date:   2018-07-25 21:30:18
+* @Last Modified by:   Charlie Gallentine
+* @Last Modified time: 2018-08-21 14:58:10
+*/
+import React, { Component } from 'react';
+import './ProjectSection.css';
+
+export default class ProjectSection extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		const project_list = document.getElementsByClassName("project_thumbnail");
+
+		for (let i = 0; i < project_list.length; i+=1) {
+		  console.log(project_list[i]);
+		  project_list[i].addEventListener("click", function() {
+		    for (let j = 0; j < project_list.length; j+=1) {
+		      if (j != i) {
+		        project_list[j].style.display = "none";
+		        document.getElementsByClassName("project")[0].style.justifyContent = "center";
+		      } else {
+		        this.nextElementSibling.style.display = "inline-block";
+		      }
+		    }
+		  }); 
+		}
+
+		document.addEventListener('click', function (event) {
+		    console.log(event.target.parentElement.parentElement.parentElement.nodeName);
+		    if (event.target.parentElement.parentElement.parentElement.nodeName == "BODY") {
+		        // Clicked outside the element...
+		      document.getElementsByClassName("project")[0].style.justifyContent = "space-around";
+		      for (let i = 0; i < project_list.length; i+=1) {
+		        project_list[i].style.display = "inline-block";
+		        project_list[i].nextElementSibling.style.display = "none";
+		      }
+		    }
+		}, false);
+
+
+		return (
+			<div className="project_list">
+				{ 
+					this.props.projects.map(function(current, index) {
+					  return <div className="project" key="index">
+					    <div className="project_thumbnail">
+					      <img src={current.image} alt={current.alt}/>
+					      <h1 style={{color: this.props.colors.color_1}}>{current.name}</h1>  
+					    </div>
+					    <div className="project_description">
+					      <p>{current.description}</p>
+					      <a 
+					      	className="project_links"
+					      	href={current.link}
+					      	style={{color: this.props.colors.color_1}}>
+					      	{current.name}
+					      </a>
+					    </div>
+					  </div> 
+				
+					  index++;
+				}, this)
+			}
+      </div>
+		);
+	}
+};
